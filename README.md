954e39397b388d65bcb1b5de4300809fe2051317
Yasuaki Uechi <y@uechi.io>
2019-01-10 00:17:08 +0900

    cosmetic changes

--- README.md
+++ README.md
@@ -4,20 +4,20 @@ Transform markdown files into IPython Notebook over the air.
 
 ## Usage
 
-Visit [nbify.herokuapp.com](http://nbify.herokuapp.com/).
+Visit [nbify.herokuapp.com](https://nbify.herokuapp.com/).
 
 ## Work with Qiita
 
-You can show Qiita articles on [nbviewer](http://nbviewer.ipython.org) through nbify([Example](http://nbviewer.ipython.org/urls/nbify.herokuapp.com/http://qiita.com/nkt_dev/items/3c66d45ca76d7f8cce19.md), [Original article](http://qiita.com/nkt_dev/items/3c66d45ca76d7f8cce19)).
+You can show Qiita articles on [nbviewer](https://nbviewer.ipython.org) through nbify([Example](https://nbviewer.ipython.org/urls/nbify.herokuapp.com/https://qiita.com/nkt_dev/items/3c66d45ca76d7f8cce19.md), [Original article](https://qiita.com/nkt_dev/items/3c66d45ca76d7f8cce19)).
 
 ```
-http://nbviewer.ipython.org/url/
+https://nbviewer.ipython.org/url/
     nbify.herokuapp.com/
-        http://path/to/markdown.md
+        https://path/to/markdown.md
 ```
 
 ↓
 
 ```
-$ open http://nbviewer.ipython.org/urls/nbify.herokuapp.com/http://path/to/markdown.md
+$ open https://nbviewer.ipython.org/urls/nbify.herokuapp.com/https://path/to/markdown.md
 ```
--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const request = require('superagent')
 const path = require('path')
-const md2ipynb = require('md2ipynb')
 const express = require('express')
+const request = require('superagent')
+const fetch = require('node-fetch')
+const md2ipynb = require('md2ipynb')
 const app = express()
 
-function getMarkdownContent(urlString, callback) {
-  request
-    .get(urlString)
-    .set('Accept', 'text/plain')
-    .end(function(err, data) {
-      if (err) {
-        callback(err, null)
-        return
-      }
-      const markdown = data.text
-      callback(null, markdown)
+async function getMarkdownContent(urlString) {
+  try {
+    const response = await fetch(urlString, {
+      headers: { Accept: 'text/plain' },
     })
+    const markdown = await response.text()
+    return markdown
+  } catch (err) {
+    throw new Error(err)
+  }
 }
 
 app.set('port', process.env.PORT || 5000)
 app.use(express.static('public'))
 
-app.get('/download/*', function(req, res, next) {
+app.get('/download/*', async (req, res, next) => {
   const markdownURL = req.params[0]
-  getMarkdownContent(markdownURL, function(err, markdown) {
-    if (err) return next(err)
+  try {
+    const markdown = await getMarkdownContent(markdownURL)
     res.type('text/plain')
     res.attachment(path.basename(markdownURL) + '.ipynb')
     res.send(md2ipynb(markdown))
-  })
+  } catch (err) {
+    next(err)
+  }
 })
 
-app.get('/*', function(req, res, next) {
-  getMarkdownContent(req.params[0], function(err, markdown) {
-    if (err) return next(err)
+app.get('/*', async (req, res, next) => {
+  try {
+    const markdown = await getMarkdownContent(req.params[0])
     res.type('text/plain')
     res.send(md2ipynb(markdown))
-  })
+  } catch (err) {
+    next(err)
+  }
 })
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   console.log(err)
   switch (err.code) {
     case 'ECONNREFUSED':
@@ -53,6 +56,6 @@ app.use(function(err, req, res, next) {
   }
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running http://localhost:' + app.get('port'))
 })
